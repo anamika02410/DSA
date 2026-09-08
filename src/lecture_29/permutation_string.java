4331c481e7d6d8ce3025a63960d79ec3b819826a
anamika02410 <kumarisinghanamika024@gmail.com>
2026-09-09 00:52:18 +0530

    added DSA question practice

---
+++ src/lecture_29/permutation_string.java
@@ -0,0 +1,20 @@
+package lecture_29;
+
+public class permutation_string {
+    public static void main(String[] args) {
+        String str="abc";
+        permutation(str,"");
+    }
+    public static void permutation(String str,String ans){
+        if(str.length()==0){
+            System.out.print(ans+" ");
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            String s1=str.substring(0,i);
+            String s2=str.substring(i+1);
+            permutation(s1+s2,ans+ch);
+        }
+    }
+}
